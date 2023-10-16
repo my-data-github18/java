667d7b4dc41bb777851d38d8ad5e3e12f2e31579
AkashBarbhai <akashbarbhai101@gmail.com>
2023-10-16 23:18:37 +0530

    Assignment2 Q3 uploaded

---
+++ Assignment2/Assignment2Q3/src/com/app/tester/DateTest.java
@@ -0,0 +1,14 @@
+package com.app.tester;
+import com.app.date.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		Date d=new Date();
+		d.accept();
+		d.display();
+		
+
+	}
+
+}
