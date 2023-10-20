44821555d1b911d9ccb28a4ef6e0a39766513868
AkashBarbhai <akashbarbhai101@gmail.com>
2023-10-20 19:04:05 +0530

    Assignment 14 Q8 uploaded

---
+++ Assignment14/Q8/src/com/sunbeam/ObserveOutput.java
@@ -0,0 +1,16 @@
+package com.sunbeam;
+
+import java.util.stream.IntStream;
+
+public class ObserveOutput {
+
+	public static void main(String[] args) {
+		IntStream s1 = IntStream.range(1, 10);
+		int sum = s1.sum();
+		System.out.println(sum);
+		
+	IntStream s2 = IntStream.range(1, 10);
+	System.out.println(s2.summaryStatistics());
+	}
+
+}
